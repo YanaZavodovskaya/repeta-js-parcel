6c136bf175d73789635f3d02495082fadd7b613a
YanaZavodovskaya <104728312+YanaZavodovskaya@users.noreply.github.com>
2022-10-01 22:10:06 +0600

    parcel

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import validatePassword from './js/validate-password';
+// import apiService from './js/api-service';
+// import {fetchAllUsers, fetchUserById, x as qweqweqwe} from './js/api-service';
+import * as apiService from './js/api-service';
+
+
+// console.log(validatePassword);
+// console.log(validatePassword('qweqweqwe'));
+// console.log(fetchAllUsers);
+// console.log(fetchUserById);
+console.log(apiService);
+
+
